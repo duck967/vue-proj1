54927bccc500f0dac62411a069f3abd12de4851f
duck967 <duck967@gmail.com>
2022-04-13 13:53:20 +0000

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+module.exports = {
+  transpileDependencies: true,
+  devServer:{
+    //disableHostCheck: true
+    allowedHosts: 'all'
+   }
+}
